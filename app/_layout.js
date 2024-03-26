a4df92487e8a512bebf682e380c00747a240d11a
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-03-25 18:20:45 -0600

    Add expo-font

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,35 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router/stack'
+import * as SplashScreen from 'expo-splash-screen'
 
 export default function AppLayout() {
+    const [isReady, setIsReady] = useState(false)
+    const [fontsLoaded, fontError] = useFonts({
+        'Roboto-Mono': require('../assets/fonts/RobotoMono.ttf'),
+    })
+
+    useEffect(() => {
+        const prepare = async () => {
+            await SplashScreen.preventAutoHideAsync()
+            setIsReady(true)
+        }
+
+        prepare()
+    }, [])
+
+    const onLayoutRootView = useCallback(async () => {
+        if (isReady) {
+            await SplashScreen.hideAsync()
+        }
+    }, [isReady])
+
+    if (!fontsLoaded && !fontError) {
+        return null
+    }
+
     return (
-        <Stack>
+        <Stack onLayout={onLayoutRootView}>
             <Stack.Screen
                 name='(tabs)'
                 options={{
