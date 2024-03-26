b05b10674051cdc5c6a2b2487f62f4cec1161181
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-03-25 21:12:47 -0600

    Update tabs layout

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,8 +1,35 @@
+import { COLORS, FONTS } from '@/constants'
 import { Tabs } from 'expo-router'
 
 export default function TabsLayout() {
     return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                headerTitleAlign: 'center',
+                headerShadowVisible: false,
+                headerTintColor: COLORS.text,
+                tabBarActiveTintColor: COLORS.primary,
+
+                headerStyle: {
+                    backgroundColor: COLORS.background,
+                },
+
+                headerTitleStyle: {
+                    fontSize: 12,
+                    letterSpacing: 1,
+                    fontFamily: FONTS.mono,
+                    textTransform: 'uppercase',
+                },
+
+                tabBarStyle: {
+                    backgroundColor: COLORS.background,
+                },
+
+                tabBarLabelStyle: {
+                    fontFamily: FONTS.mono,
+                },
+            }}
+        >
             <Tabs.Screen
                 name='index'
                 options={{
