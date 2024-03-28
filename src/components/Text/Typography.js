0ab714fa9907b224d00147df96c7cb571aea0857
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-03-27 20:19:14 -0600

    Add typography component

---
+++ src/components/Text/Typography.js
@@ -0,0 +1,50 @@
+import { StyleSheet, Text } from 'react-native'
+import { COLORS, FONTS } from '@/constants'
+
+export function Typography({ children, variant = 'paragraph', bold, uppercase, color, opacity, textAlign, ...props }) {
+    const variantStyles = styles[variant]
+
+    return (
+        <Text
+            {...props}
+            style={[
+                variantStyles,
+                bold && styles.bold,
+                uppercase && styles.uppercase,
+                color && { color },
+                opacity && { opacity },
+                textAlign && { textAlign },
+            ]}>
+            {children}
+        </Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 18,
+        color: COLORS.text,
+        fontFamily: FONTS.mono,
+    },
+    subtitle: {
+        fontSize: 16,
+        color: COLORS.text,
+        fontFamily: FONTS.mono,
+    },
+    paragraph: {
+        fontSize: 14,
+        color: COLORS.text,
+        fontFamily: FONTS.mono,
+    },
+    caption: {
+        fontSize: 12,
+        color: COLORS.text,
+        fontFamily: FONTS.mono,
+    },
+    uppercase: {
+        textTransform: 'uppercase',
+    },
+    bold: {
+        fontWeight: 'bold',
+    },
+})
