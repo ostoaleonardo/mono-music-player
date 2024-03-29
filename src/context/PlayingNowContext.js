dd364df9c65d65b2760e95ec83484e67af001f0b
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-03-28 20:43:17 -0600

    Update playing now context

--- src/context/PlayingNowContext.js
+++ src/context/PlayingNowContext.js
@@ -3,18 +3,22 @@ import { createContext, useState } from 'react'
 export const PlayingNowContext = createContext()
 
 export function PlayingNowProvider({ children }) {
+    const [sound, setSound] = useState(null)
     const [isPlaying, setIsPlaying] = useState(false)
     const [playingNow, setPlayingNow] = useState({
         id: null,
         title: 'Play a track',
         artist: 'No artist',
+        uri: null,
     })
 
     return (
         <PlayingNowContext.Provider
             value={{
+                sound,
                 isPlaying,
                 playingNow,
+                setSound,
                 setIsPlaying,
                 setPlayingNow
             }}
--- src/hooks/usePlayingNow.js
+++ src/hooks/usePlayingNow.js
@@ -3,15 +3,19 @@ import { PlayingNowContext } from '@/context/PlayingNowContext'
 
 export function usePlayingNow() {
     const {
+        sound,
         isPlaying,
         playingNow,
+        setSound,
         setIsPlaying,
         setPlayingNow
     } = useContext(PlayingNowContext)
 
     return {
+        sound,
         isPlaying,
         playingNow,
+        setSound,
         setPlayingNow,
         setIsPlaying,
     }
