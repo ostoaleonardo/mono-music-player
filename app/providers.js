a0a6b65ad9dd5adaff6bc7b4689dccc3c6df9b03
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-03-29 18:41:56 -0600

    Add dependencies

--- app/providers.js
+++ app/providers.js
@@ -1,9 +1,12 @@
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { PlayingNowProvider } from '@/context/PlayingNowContext'
 
 export function Providers({ children }) {
     return (
-        <PlayingNowProvider>
-            {children}
-        </PlayingNowProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <PlayingNowProvider>
+                {children}
+            </PlayingNowProvider>
+        </GestureHandlerRootView>
     )
 }
